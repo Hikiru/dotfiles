3bdfd2b33f2859d95efe8277bb225da92fe069f7
Hikiru <hikiruu@proton.me>
2025-11-26 10:57:13 -0600

    move alias in fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias sync='chezmoi git pull && chezmoi apply && cd ~/nix-config && git pull && just'
     set -g fish_greeting
     starship init fish | source
 end
--- dot_config/private_fish/functions/sync.fish
+++
@@ -1,3 +0,0 @@
-function sync --wraps='chezmoi git pull && chezmoi apply && cd ~/nix-config && git pull && just all' --description 'alias sync=chezmoi git pull && chezmoi apply && cd ~/nix-config && git pull && just all'
-    chezmoi git pull && chezmoi apply && cd ~/nix-config && git pull && just all $argv
-end
