32233f5e841357a101cc8ea62a1da8e8380981d6
Hikiru <hikiruu@proton.me>
2025-11-30 15:26:14 -0600

    distrobox alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias sync='chezmoi git pull && chezmoi apply && cd ~/nix-config && git pull && just'
+    alias dbu='distrobox enter --root ubuntu'
     set -g fish_greeting
     starship init fish | source
 end
